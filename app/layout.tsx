789f85cad971e4afac0a5f6f9dab08d0068ea2ff
meitekini <meitekinileakey@gmail.com>
2026-02-04 08:24:55 +0000

    Install and set up clerk js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 import ScrollToTop from "@/components/shared/scroll-to-top";
 
@@ -24,6 +25,7 @@ export default function AppLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -32,5 +34,6 @@ export default function AppLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
