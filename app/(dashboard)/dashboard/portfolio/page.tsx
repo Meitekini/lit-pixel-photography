c2f553d0d9efa1bbb79ee54067a2478e7080fab7
meitekini <meitekinileakey@gmail.com>
2026-02-16 06:35:25 +0000

    test vercel blob upload api

--- app/(dashboard)/dashboard/portfolio/page.tsx
+++ app/(dashboard)/dashboard/portfolio/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import TestUpload from "@/components/dashboard/test-upload";
+
 
 export default function page() {
   return (
-    <div>Portfolio Page</div>
+    <div>
+      <TestUpload />
+    </div>
   )
 }
